964e1fd75fcb46d0a602c334fe447eae50a9ca31
Austin Wood <i@austinwood.me>
2015-10-14 09:01:31 -0600

    Include specs for sort challenge

---
+++ examples/05-sort/test.js
@@ -0,0 +1,11 @@
+describe('popularPhotos', function () {
+  it('is an array', function () {
+    assert(Array.isArray(popularPhotos));
+  });
+
+  it('sorts the photos by their number of likes', function () {
+    for (var i = 0; i < popularPhotos.length - 1; i++) {
+      assert.operator(popularPhotos[i].likes.count, '<=', popularPhotos[i+1].likes.count);
+    }
+  });
+});
