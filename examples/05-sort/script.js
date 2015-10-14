96ad038be9d2dc8e33a11939c6f18164a957a429
Austin Wood <i@austinwood.me>
2015-10-14 10:05:56 -0600

    Include TODO helper in script for sort

---
+++ examples/05-sort/script.js
@@ -0,0 +1,4 @@
+// TODO: Sort the photos and videos by the number of times it was liked.
+// The most liked photo/video should be first and the least liked photo/video should be last.
+
+var popularPhotos = photographs;
